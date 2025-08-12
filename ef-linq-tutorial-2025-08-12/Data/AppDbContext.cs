18f5650a0ca0b8901c58ba99a1c1bf8471be6720
JohnRowleyDorsetCollege <john.rowley@dorset.ie>
2025-08-12 19:20:54 +0100

    Added on model building

--- ef-linq-tutorial-2025-08-12/Data/AppDbContext.cs
+++ ef-linq-tutorial-2025-08-12/Data/AppDbContext.cs
@@ -12,5 +12,60 @@ namespace ef_linq_tutorial_2025_08_12.Data
         public DbSet<Enrollment> Enrollments => Set<Enrollment>();
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(b =>
+            {
+                b.OwnsOne(s => s.Name, nb =>
+                {
+                    nb.Property(p => p.First).HasMaxLength(50);
+                    nb.Property(p => p.Last).HasMaxLength(50);
+                });
+                b.OwnsOne(s => s.Address, ab =>
+                {
+                    ab.Property(p => p.Street).HasMaxLength(100);
+                    ab.Property(p => p.City).HasMaxLength(50);
+                    ab.Property(p => p.Country).HasMaxLength(50);
+                });
+                b.HasIndex(s => new { s.Name.Last, s.Name.First });
+            });
+
+            modelBuilder.Entity<Instructor>(b =>
+            {
+                b.OwnsOne(i => i.Name);
+                b.OwnsOne(i => i.Address);
+            });
+
+            modelBuilder.Entity<Department>(b =>
+            {
+                b.Property(d => d.Budget).HasColumnType("decimal(12,2)");
+                b.HasIndex(d => d.Name).IsUnique();
+            });
+
+            modelBuilder.Entity<Course>(b =>
+            {
+                b.Property(c => c.Title).HasMaxLength(100);
+                b.HasOne(c => c.Department)
+                    .WithMany(d => d.Courses)
+                    .HasForeignKey(c => c.DepartmentId)
+                    .OnDelete(DeleteBehavior.Restrict);
+                b.HasOne(c => c.Instructor)
+                    .WithMany(i => i.Courses)
+                    .HasForeignKey(c => c.InstructorId);
+            });
+
+            modelBuilder.Entity<Enrollment>(b =>
+            {
+                b.HasKey(e => new { e.StudentId, e.CourseId });
+                b.HasOne(e => e.Student)
+                    .WithMany(s => s.Enrollments)
+                    .HasForeignKey(e => e.StudentId);
+                b.HasOne(e => e.Course)
+                    .WithMany(c => c.Enrollments)
+                    .HasForeignKey(e => e.CourseId);
+            });
+        }
     }
 }
